341733fb9fbd13f5cd96abf94f13b52d62d9eba1
JCTsai0 <JCTsai0@winbond.com>
2024-04-08 21:55:24 +0800

    Version 0.0.1

---
+++ Planning_main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "process.h"
+using std::cout, std::endl;
+
+int main() {
+    cout << "Planning starts!" << endl;
+    Process my_process;
+    my_process.planProcess();
+    cout << "Planning ends!" << endl;
+
+    return 0;
+}
